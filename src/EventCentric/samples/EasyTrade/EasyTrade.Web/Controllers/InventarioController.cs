e6e7489702ea5c4c670986ddfff7c22dcbecc206
Alexis Narváez <narvalex@hotmail.com>
2015-09-04 19:54:40 -0400

    Se agrego al menu la seccion de Clientes

---
+++ src/EventCentric/samples/EasyTrade/EasyTrade.Web/Controllers/InventarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyTrade.Web.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/EventCentric/samples/EasyTrade/EasyTrade.Web/app/clientes/nuevoClienteController.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('nuevoClienteController', nuevoClienteController);
+
+    nuevoClienteController.$inject = [];
+
+    function nuevoClienteController() {
+
+        activate();
+
+        function activate() {
+
+        }
+    }
+})();
