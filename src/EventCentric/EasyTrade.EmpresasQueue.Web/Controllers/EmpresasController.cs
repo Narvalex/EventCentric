f7ed0818b2b82a04f3ce66b7bcfa96152e152e13
Alexis Narváez <narvalex@hotmail.com>
2015-09-08 07:47:31 -0400

    CHECKPOINT

---
+++ src/EventCentric/EasyTrade.EmpresasQueue.Web/Controllers/EmpresasController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace EasyTrade.EmpresasQueue.Web.Controllers
+{
+    public class EmpresasController : Controller
+    {
+        // GET: Empresas
+        [HttpPost]
+        [Route("empresas/nueva-empresa")]
+        public ActionResult NuevaEmpresa(NuevaEmpresaDto empresa)
+        {
+            return this.Content("Hola");
+        }
+    }
+    public class NuevaEmpresaDto
+    {
+        public string Nombre { get; set; }
+        public string RUC { get; set; }
+        public string Descripcion { get; set; }
+    }
+
+}
