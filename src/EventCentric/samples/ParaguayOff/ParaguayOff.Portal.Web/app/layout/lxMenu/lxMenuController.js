a7e8a8092d85c0d5544eddcc4082eafd6e76c153
Alexis Narváez <narvalex@hotmail.com>
2015-10-02 15:05:57 -0400

    Starting to develop the UI of Paraguay Off

---
+++ src/EventCentric/samples/ParaguayOff/ParaguayOff.Portal.Web/app/layout/lxMenu/lxMenuController.js
@@ -0,0 +1,30 @@
+﻿'use strict';
+
+angular.module('lxMenu').controller('lxMenuController',
+    ['$scope', '$rootScope',
+        function ($scope, $rootScope) {
+
+            this.getActiveItem = function () {
+                return $scope.activeElement;
+            };
+
+            this.setActiveItem = function (el) {
+                $scope.activeElement = el;
+            };
+
+            this.setRoute = function (route) {
+                $rootScope.$broadcast('lxMessage-itemSelected',
+                {
+                    route: route
+                });
+            };
+
+            this.setOpenMenuScope = function (scope) {
+                $scope.openMenuScope = scope;
+            }
+
+            $scope.$on('lxMessage-showMenuStateChanged', function (event, data) {
+                $scope.showMenu = data.show;
+            });
+        }
+    ]);
