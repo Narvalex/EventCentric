2035ffbf51f1c54314138eab67e2bad3c14036b4
Alexis Narváez <narvalex@hotmail.com>
2015-10-02 15:31:45 -0400

    Route implemented in Paraguay Off

---
+++ src/EventCentric/samples/ParaguayOff/ParaguayOff.Portal.Web/app/appModule.js
@@ -0,0 +1,18 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', [
+        'lxLayout',         // Lex Layout
+        'ngRoute',          // Routing
+
+        // 3rd party modules
+    ]);
+
+    app.run(['$route', 'routeMediator',
+        function ($route, routeMediator) {
+            // Include $route to kick start the router.
+            routeMediator.setRoutingHandlers();
+        }
+    ]);
+
+})();
