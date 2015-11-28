e3ac98c1a2363e68e5d1e59ada3a56a86554950e
Alexis Narváez <narvalex@hotmail.com>
2015-11-28 20:16:47 -0300

    Double checking made to node initializer

--- src/EventCentric/EventCentric/Node/NodeInitializer.cs
+++ src/EventCentric/EventCentric/Node/NodeInitializer.cs
@@ -8,17 +8,20 @@ namespace EventCentric
     {
         private static object _lockObject = new object();
         private static INode _node = null;
+        private static bool isRunning = false;
 
         public static void Initialize(Func<INode> nodeFactory)
         {
             lock (_lockObject)
             {
-                if (_node != null)
+                // Double checking
+                if (_node != null || isRunning)
                     return;
 
                 DbConfiguration.SetConfiguration(new TransientFaultHandlingDbConfiguration());
                 _node = nodeFactory.Invoke();
                 _node.Start();
+                isRunning = true;
             }
         }
     }
