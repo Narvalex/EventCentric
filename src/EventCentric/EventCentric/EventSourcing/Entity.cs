709919cd2453a654955e6320b0212a69db72c52a
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2016-02-06 15:40:00 -0300

    Improving memento

--- src/EventCentric/EventCentric/EventSourcing/Entity.cs
+++ src/EventCentric/EventCentric/EventSourcing/Entity.cs
@@ -1,7 +1,19 @@
-﻿namespace EventCentric.EventSourcing
-{
-    public abstract class Entity
-    {
+﻿using System;
 
+namespace EventCentric.EventSourcing
+{
+    public abstract class Entity<T> : IMementoOriginator<T> where T : IMemento
+    {
+        public abstract T SaveToMemento();
+
+        protected Entity(Guid id, Action<Event> update, T memento) // to recall to bring the memento in construction time
+        {
+            this.Id = id;
+            this.Update = update;
+        }
+
+        protected Guid Id { get; }
+
+        protected Action<Event> Update { get; }
     }
 }
---
+++ src/EventCentric/EventCentric/EventSourcing/IMementoOriginator.cs
@@ -0,0 +1,12 @@
+﻿namespace EventCentric.EventSourcing
+{
+    public interface IMementoOriginator<T> where T : IMemento
+    {
+        T SaveToMemento();
+    }
+
+    public interface IMemento { }
+
+    public class VoidMemento : IMemento
+    { }
+}
