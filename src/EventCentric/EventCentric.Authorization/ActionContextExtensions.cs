af83c9f6734527f1f61ac6b30a4c7d98c7e1f4e2
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2016-02-08 10:12:15 -0300

    Action context extensions

---
+++ src/EventCentric/EventCentric.Authorization/ActionContextExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.Web.Http.Controllers
+{
+    public static class ActionContextExtensions
+    {
+        /// <summary>
+        /// Usability overload that is usefull to retrieve the token of the current user.
+        /// </summary>
+        public static string GetToken(this HttpActionContext context)
+        {
+            IEnumerable<string> values;
+            return context.Request.Headers.TryGetValues("Authorization", out values) ? values.First() : null;
+        }
+    }
+}
