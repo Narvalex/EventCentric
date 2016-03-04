f39ae15ef5c9e578a461a563c1b69ca3b2d5b629
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2016-03-04 15:14:14 -0300

    bench test... all passing

--- src/EventCentric/PersistenceBenchmark.Console/Program.cs
+++ src/EventCentric/PersistenceBenchmark.Console/Program.cs
@@ -14,8 +14,8 @@ namespace PersistenceBenchmark.ConsoleHost
             var user2App = UnityConfig.UserContainer2.Resolve<UserAppService>();
 
             // Holding in memory messages
-            user1App.StressWithWavesOfConcurrentUsers(wavesCount: 2, concurrentUsers: 1000);
-            user2App.StressWithWavesOfConcurrentUsers(wavesCount: 2, concurrentUsers: 1000);
+            user1App.StressWithWavesOfConcurrentUsers(wavesCount: 1, concurrentUsers: 2000);
+            user2App.StressWithWavesOfConcurrentUsers(wavesCount: 1, concurrentUsers: 2000);
 
             Console.WriteLine("Press any key to stop");
             Console.ReadLine();
