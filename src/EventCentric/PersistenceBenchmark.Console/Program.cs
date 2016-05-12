e1b0ca83883467b19e3fa8eba0ebc9f99925e185
Alexis Darién Narváez Gamarra <narvalex@hotmail.com>
2016-05-12 10:58:23 -0400

    updated bench definition

--- src/EventCentric/PersistenceBenchmark.Console/Program.cs
+++ src/EventCentric/PersistenceBenchmark.Console/Program.cs
@@ -26,21 +26,14 @@ namespace PersistenceBenchmark.ConsoleHost
             Console.WriteLine("Press enter to start....");
             Console.ReadLine();
 
-            // THIS MAKE CRASH, ALMOST
 
-            // SQL SERVER ENTITY FRAMEWORK -----------------------------------
-            // 50 througput,    completes in 2:48 m 10.000 messages    60 m/s
-            // 100 througput,   completes in 4:44 m 20.000 messages    70 m/s
-
-            // SQL SERVER ADO.NET ---------------------------------------------
-            // 100 througput,   completes in 1:48 m 20.000 messages    185 m/s
-            // SQL SERVER ADO.NET SSD -----------------------------------------
-            // 200 througput,   completes in 38 s 10.000 messages    263 m/s
-            //user1App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 500);
-            //user2App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 500);
+            // SQL SERVER ADO.NET --------------------------------------------
+            // 100 througput,   completes in 38 s 10.000 messages    263 m/s
+            //user1App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 1000);
+            //user2App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 1000);
 
             // IN-MEMORY-------------------------------------------------------
-            // 100 througput,   completes in 0:20 s 16.900 messgaes    845 m/s
+            // 100 througput,   completes in 0:20 s 17.204 messgaes    860 m/s
             user1App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 1000);
             user2App.StressWithWavesOfConcurrentUsers(wavesCount: 5, concurrentUsers: 1000);
 
